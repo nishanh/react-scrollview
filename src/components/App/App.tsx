638e252bb0bc6cac1c95b3aa2b22e01dd64ae2a9
Nishan Hossepian <nhossep@gmail.com>
2019-06-11 13:20:48 -0700

    Add tree control to demonstrate content resize

--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -6,59 +6,99 @@
 import React from 'react';
 import styles from './App.module.scss';
 import { ScrollView } from '..';
+import { Tree, ITreeNode } from '@blueprintjs/core';
+
+/*************************************/
+/*         Types / Interfaces        */
+/*************************************/
+
+interface IState {
+  treeNodes: ITreeNode[];
+}
 
 /*************************************/
 /*        Component Definition       */
 /*************************************/
 
-export const App: React.FC = () => {
-  return (
-    <div className={styles.App}>
-      <div className={styles.primaryPanel}>
-        <div className={styles.topSubPanel}>
-          <ScrollView>
-            <div>
-              <div>Item 1</div>
-              <div>Item 2</div>
-              <div>Item 3</div>
-              <div>Item 4</div>
-              <div>Long Item 123456789012345678901234567890</div>
-              <div>Item 5</div>
-              <div>Item 6</div>
-              <div>Item 7</div>
-              <div>Item 8</div>
-              <div>Item 9</div>
-              <div>
-                <input type="checkbox"/>
-                Test
-              </div>
-              <div>Item 10</div>
-              <div>Item 11</div>
-            </div>
-          </ScrollView>
-        </div>
-        <div className={styles.bottomSubPanel}>
-          <ScrollView allowWrap>
-            <div>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
-              Sed auctor posuere mi id egestas. Class aptent taciti sociosqu ad litora 
-              torquent per conubia nostra, per inceptos himenaeos. Cras nec vestibulum nisi. 
-              Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
-              Morbi ultrices at tortor eu ultrices. Nullam ut tellus blandit, ultricies 
-              purus eu, laoreet nibh. Fusce id dignissim urna. Nulla ut est nec ex elementum ullamcorper. 
-              Morbi eget lacinia nisi, non tempor erat. In lacinia ante est, sed feugiat justo euismod vel.               
-            </div>
-          </ScrollView>
-        </div>
+export class App extends React.Component<any, IState> {
+  public state: IState = { treeNodes: [rootNode] };
 
-      </div>
-      <div className={styles.primaryPanel}>
-        <ScrollView wheelDelta={5}> 
-          <div>
-            <img src="./assets/aliens.png" alt="Aliens!"/>
+  public render(): JSX.Element {
+    return (
+      <div className={styles.App}>
+        <div className={styles.primaryPanel}>
+          <div className={styles.topSubPanel}>
+            <ScrollView>
+             <Tree 
+               contents={this.state.treeNodes}
+               onNodeCollapse={this.handleNodeCollapse}
+               onNodeExpand={this.handleNodeExpand}/>
+            </ScrollView>
           </div>
-        </ScrollView>
+          <div className={styles.bottomSubPanel}>
+            <ScrollView allowWrap>
+                Lorem ipsum dolor sit amet, consectetur adipiscing elit. 
+                Sed auctor posuere mi id egestas. Class aptent taciti sociosqu ad litora 
+                torquent per conubia nostra, per inceptos himenaeos. Cras nec vestibulum nisi. 
+                Vestibulum ante ipsum primis in faucibus orci luctus et ultrices posuere cubilia Curae; 
+                Morbi ultrices at tortor eu ultrices. Nullam ut tellus blandit, ultricies 
+                purus eu, laoreet nibh. Fusce id dignissim urna. Nulla ut est nec ex elementum ullamcorper. 
+                Morbi eget lacinia nisi, non tempor erat. In lacinia ante est, sed feugiat justo euismod vel.               
+            </ScrollView>
+          </div>
+        </div>
+        <div className={styles.primaryPanel}>
+          <ScrollView wheelDelta={5}> 
+            <img src="./assets/aliens.png" alt="Aliens!"/>
+          </ScrollView>
+        </div>
       </div>
-    </div>
-  );
+    );  
+  }
+
+  private handleNodeCollapse = (nodeData: ITreeNode): void => {
+    nodeData.isExpanded = false;
+    nodeData.icon = 'folder-close';
+    this.setState(this.state);
+  }
+
+  private handleNodeExpand = (nodeData: ITreeNode): void => {
+    nodeData.isExpanded = true;
+    nodeData.icon = 'folder-open';
+    this.setState(this.state);
+  }
+}
+
+/*************************************/
+/*            Sample Data            */
+/*************************************/
+
+const rootNode: ITreeNode = {
+  id: 1,
+  label: 'Root',
+  icon: 'folder-open',
+  isExpanded: true,
+  childNodes: [
+    { id: 2, label: 'Item 1', icon: 'document'},
+    { id: 3, label: 'Item 2', icon: 'document'},
+    { id: 4, label: 'Item 3', icon: 'folder-close', childNodes: [
+      {id: 41, label: 'Sub Item 1', icon: 'document'},
+      {id: 42, label: 'Sub Item 2', icon: 'document'},
+      {id: 43, label: 'Sub Item 3', icon: 'document'},
+      {id: 44, label: 'Sub Item 4', icon: 'document'},
+      {id: 45, label: 'Sub Item 5', icon: 'document'},
+      {id: 46, label: 'Sub Item 6', icon: 'document'},
+      {id: 47, label: 'Sub Item 7', icon: 'document'},
+      {id: 48, label: 'Sub Item 8', icon: 'document'},
+      {id: 49, label: 'Sub Item 8', icon: 'document'},
+      {id: 50, label: 'Sub Item 10', icon: 'document'},
+    ]},
+    { id: 5, label: 'Item 4', icon: 'document'},
+    { id: 6, label: 'Item 5', icon: 'document'},
+    { id: 7, label: 'Item 6', icon: 'document'},
+    { id: 8, label: 'Item 7', icon: 'document'},
+    { id: 9, label: 'Item 8', icon: 'document'},
+    { id: 10, label: 'Item 9', icon: 'document'},
+    { id: 11, label: 'Item 10', icon: 'document'},
+  ]
 };
